ffb4f5e5f0bb9df37b1fff87fecd59018e6298d6
rachaeltesting <leahxraexcar@gmail.com>
2016-06-01 11:00:45 -0400

    fix

--- modules/simon.js
+++ modules/simon.js
@@ -1,10 +1,13 @@
 function simon(color, strikes) {
-	if ($('#vowelyes').hasClass('active')) {
+	console.log(strikes);
+	if ($('#vowelyes').hasClass('btn-primary')) {
 		var vowel = true;
 	} else {vowel = false}
+	console.log(vowel);
 
 	if ((strikes == 0 && color == "Red") ||
 	 (vowel && strikes == 1 && color == "Green") ||
+	 (!vowel && strikes == 1 && color == "Blue") ||
 	  (vowel && color == "Yellow" && strikes == 2) ||
 	   (!vowel && strikes == 0 && color == "Red") ||
 	    (!vowel && strikes == 2 && color == "Green")) {
@@ -22,7 +25,7 @@ function simon(color, strikes) {
 		(!vowel && strikes == 0 && color == "Blue") ||
 		(!vowel && strikes == 1 && color == "Green") ||
 		(!vowel && strikes == 2 && color == "Red")
-		) {
+		) { console.log('yellow');
 		return "Yellow"
 	} else {return "Green"}
 
